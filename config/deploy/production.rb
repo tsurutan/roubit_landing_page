3be1043cb48ec1be6effaed3a0a93c5d6c0c855c
tsurutan <ishaninaritaina623@gmail.com>
2017-11-05 20:12:12 +0900

    add unko

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,9 @@
+# EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
+server '54.178.163.161', user: 'tsurutan', roles: %w{app db web}
+
+#デプロイするサーバーにsshログインする鍵の情報を記述
+set :ssh_options, keys: '~/.ssh/id_rsa'
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
